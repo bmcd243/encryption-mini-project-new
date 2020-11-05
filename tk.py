8e797f71411f00148a12911290de82344615ebdb
bmcd243 <64990696+bmcd243@users.noreply.github.com>
2020-11-05 13:19:21 +0000

    Update tk.py

--- tk.py
+++ tk.py
@@ -3,19 +3,58 @@ from tkinter import *
 root = Tk()
 root.geometry('400x400')
 
-def chosen_encryption():
-    e = Entry(root)
-    enter_encryption = Label(root, text="Choose your type of encryption: ", command=encrypt)  
+
+
 
 def enter():
-    enter_sentence = Label(root, text="Please enter a word/sentence: ", command=encrypt)
-    e = Entry(root)
-    choose_caeser = Label(root, command=caeser)
-    e.pack()
-    choose_caeser.pack()
-
-def choose_caeser():
+    global L
     
+    enter_sentence = Label(root, text="Please enter a word/sentence: ")
+    enter_sentence.pack()
+    e = Entry(root)
+    e.pack()
+    choose_caeser = Button(root, command=caeser, text='caeser')
+    choose_vernam = Button(root, command=vernam, text='virnam')
+    choose_caeser.pack()
+    choose_vernam.pack()
+    
+
+    word=e.get()
+    L = list(word)
+
+def caeser():
+    a=1
+    b=2
+    c=3
+    d=4
+    e=5
+    f=6
+    g=7
+    h=8
+    i=9
+    j=10
+    k=11
+    l=12
+    m=13
+    n=14
+    n=15
+    o=16
+    p=17
+    q=18
+    r=18
+    s=19
+    t=20
+    u=21
+    v=22
+    w=23
+    x=24
+    y=25
+    z=26
+
+    print (L)
+
+def vernam():
+    print('')
 
 enter()
 
